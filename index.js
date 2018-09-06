ff880778f57b1f602f0ed6b60ec011275be72fe7
viqi zevic <vicky.tanzil@gmail.com>
2018-09-06 21:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length-1)
 }
 
-function removeFirstKitten() {
-  return kittens.slice(1)
+function removeFirstKitten(name) {
+  let arr = name.slice(1)
+  return arr
 }
